613b2a2d49b05927d546439dc5d0db47183fd7c9
abhishekRIgit <abhishek.anand@river-island.com>
2022-04-09 17:46:26 +0100

    testing snowflake operator

---
+++ dags/snowflake.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.snowflake.operatiors.snowflake import snowflake
+
+from datetime import datetime
+
+with DAG("my_dag",start_date=datetime(2021,1,1),schedule_interval='@daily',catchup=False) as dag:
+
+    task_a=DummyOperator(
+        task_id="task_a"
+    )
+
+    task_b=DummyOperator(
+        task_id="task_b"
+    )
+
+    task_a >> task_b
