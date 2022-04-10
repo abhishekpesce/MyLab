c70714f69635c8bdb2db730bfce27702d3899524
abhishekRIgit <abhishek.anand@river-island.com>
2022-04-10 20:22:24 +0100

    snowflake file changed

--- dags/snowflake.py
+++ dags/snowflake.py
@@ -4,14 +4,19 @@ from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 
 from datetime import datetime
 
+query_test = """select current_date() as date;"""
+
 with DAG("snowflake",start_date=datetime(2021,1,1),schedule_interval='@daily',catchup=False) as dag:
+
+    snowflake = SnowflakeOperator(
+    task_id='test_snowflake_connection',
+    sql=query_test,
+    snowflake_conn_id='snowflake_conn',
+    dag=dag
+    )
 
     task_a=DummyOperator(
         task_id="task_a"
     )
 
-    task_b=DummyOperator(
-        task_id="task_b"
-    )
-
-    task_a >> task_b
+task_a >> snowflake
