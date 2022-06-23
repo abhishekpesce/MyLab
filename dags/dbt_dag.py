b6a7d107f13f02bce7171b32740092b5af32e826
abhishekRIgit <abhishek.anand@river-island.com>
2022-06-23 15:52:41 +0100

    file added

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -8,7 +8,7 @@ from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
 from airflow.utils.edgemodifier import Label
 
 DBT_CLOUD_CONN_ID = "dbt_cloud"
-JOB_ID = 98266
+JOB_ID = "{{ var.value.dbt_cloud_job_id }}"
 
 def _check_job_not_running(job_id):
     """
