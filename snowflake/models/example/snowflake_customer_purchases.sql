ed75369071046f961f7d201413e23dbbee1caa98
abhishekRIgit <abhishek.anand@river-island.com>
2022-07-04 16:45:50 +0100

    file added

---
+++ snowflake/models/example/snowflake_customer_purchases.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c.custkey,
+    c.c_name,
+    c.c_nationkey as nation,
+    sum(o.o_totalprice) as total_order_price
+from {{ source('sample','customer')}} c
+LEFT JOIN {{ source('sample','orders')}} o
+ON c.custkey = o.o_custkey
+group by 
+    c.c_custkey,
+    c.c_name,
+    c.c_nationkey
